3a49a4983816f0d0b0c35f29499d9f85357944c8
ValerioViteritti <vviteritti@gmail.com>
2024-09-09 16:01:52 +0200

    feat: add nav-bar

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -14,14 +14,26 @@
 
 <body>
     <header>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Navbar</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav">
+                        <a class="nav-link active" aria-current="page" href="/">Home</a>
+                        <a class="nav-link" href="/about">About</a>
+                    </div>
+                </div>
+            </div>
+        </nav>
 
     </header>
     <main>
         <h3 class="text-center">{{ $prova }}</h3>
-
-
-
-        <a href="/">Torna alla Home</a>
     </main>
     <footer>
         <!-- place footer here -->
